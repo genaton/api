9850f486cb53f101d41c2714ea446930e44c8c49
genaton <genaton@gmail.com>
2023-10-14 17:26:11 -0300

    cria DTO DadosListagemMedico para devolver dados requisitados para consulta. Acidionado metodo na classe MedicoController com verbo get para devolver, via json, os dados do DTO criado

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,14 +3,14 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController // anotacao necessaria para o Spring carregar a classe
 @RequestMapping("/medicos")
@@ -28,8 +28,10 @@ public class MedicoController {
      public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){ // anotacao @RequestBody. Para receber as conteudo json fragmentado eh necessario criar classes que contenham metodos com paramentros com nomes identicos (case sensitive) aos ados contidos no json
 //        System.out.println(dados);
        repository.save(new Medico(dados));
-
-
+    }
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
 }

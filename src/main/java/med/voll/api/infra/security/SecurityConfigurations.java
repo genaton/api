b446ba2644f5622218deacb702cc37a8eb49574d
genaton <genaton@gmail.com>
2023-10-16 21:35:37 -0300

    Implementando autenticacao e autorizacao Cria controller para autenticacao do usuario AutenticacaoController Cria DTO DadosAutenticacao implementa metodos para criptografar senha usando BCcrypt e gerenciamento de senha implementa interface UserDetails na classe Usuarios para controlar o login e detalhar o usuario.

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,9 +2,13 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 //classe que desabilita o padrao spring security para personalizacao
@@ -16,6 +20,15 @@ public class SecurityConfigurations {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+    @Bean // serve para exportar uma classe para o Spring, fazendo com que ele consiga carrega-la e realize a sua injecao de dependencia em outras classes.
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 
